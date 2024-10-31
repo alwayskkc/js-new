799ae270fcc8148fa06e6d9b58f298a2100348b1
alwayskkc <61517325+alwayskkc@users.noreply.github.com>
2024-10-31 21:49:31 +0000

    font size change and added files in basics folder

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "palvadi.krish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Cincinnati"
+let accountState;
+//console.log(accountId);
+accountEmail = "p@p.com"
+accountPassword = "1"
+accountCity = "Cincy"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use "var"
+because of issue in block scope and functional scope
+*/
