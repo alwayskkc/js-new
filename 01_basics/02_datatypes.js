3b2fa4818537b9596c773b8ab6b9027c4fc5708a
alwayskkc <61517325+alwayskkc@users.noreply.github.com>
2024-10-31 22:19:49 +0000

    added datatypes file

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treats JS code to newer version
+
+//alert (3+3) // This kind of alert is used in the browser, not in node js
+
+console.log(3
+    +
+    3
+) // code readability should be high
+
+console.log("Chaitu");
+
+let name = "Chaitu"
+let age = 25
+let isLoggedIn = false
+
+// PRIMITIVE DATATYPES
+// range of number => 2^53
+// bigint
+// string => "" or ''
+// boolean
+// null => standalone value (empty value) [object datatype]
+// undefined => uninitialized 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);
