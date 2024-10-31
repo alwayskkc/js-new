dfd01c5259d824708cbc44115173419fe76c7690
alwayskkc <61517325+alwayskkc@users.noreply.github.com>
2024-10-31 23:13:40 +0000

    added datatypes-summary file

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,10 @@
+// PRIMITIVE DATATYPES => call by value
+
+// 7 types: String, Number, Boolean, BigInt, null, undefined, Symbol
+
+// Java Script is dynamically typed language
+
+
+// REFERENCE (Non-primitive) => call by reference
+
+// Array, Objects, Functions
