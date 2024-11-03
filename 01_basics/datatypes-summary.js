fb52ff1d37e48b3af468e2bda16aec238e63da97
alwayskkc <61517325+alwayskkc@users.noreply.github.com>
2024-11-03 01:42:18 +0000

    updated datatypes-summary file

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -8,3 +8,28 @@
 // REFERENCE (Non-primitive) => call by reference
 
 // Array, Objects, Functions
+/*
+const id = Symbol('123')
+const iid = Symbol('123')
+*/
+//console.table(id == iid)
+
+//const bigNumber = 8893484983409384093n
+
+//console.log(typeof bigNumber)
+
+const heroes = ["Ironman", "Wanda", "Cap"]
+let myObj = {
+    name: "Chaitu",
+    age: 25
+}
+console.log(myObj.name)
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof myObj);
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
