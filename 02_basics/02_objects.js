e0fcaa16d10ff2c797a3d460cbfe949cb1685e21
alwayskkc <61517325+alwayskkc@users.noreply.github.com>
2024-11-07 22:55:38 +0000

    objects done

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,140 @@
+/*
+// singleton
+// Object.create
+
+// Object Literals
+const mySym = Symbol("key1")
+const user = {
+    0: "Kesava",
+    age: 23,
+    [mySym]: "mykey1"
+}
+console.log(user[0]);
+console.log(user.age);
+console.log(user["age"]);
+console.log(typeof user);
+console.log(Object.keys(user));
+console.log(user[mySym]);
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+//JsUser.idea = Object.create({});
+JsUser.idea = {};
+JsUser.idea.first = "hey";
+
+//console.log(JsUser.greeting());
+//console.log(JsUser.greetingTwo());
+
+//console.log(JsUser);
+
+const existingUser = { idea: { first: "hey" } };
+const JsUser1 = { 
+  ...existingUser, 
+  idea: { 
+    ...existingUser.idea, // Spread the original properties of idea
+    second: "hello"       // Add the new key-value pair
+  },
+  age: 30
+};
+
+console.log(JsUser1); 
+
+*/
+
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
