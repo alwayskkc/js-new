14953dd9e7a5f6b34775af778a75d893dcf87fd1
alwayskkc <61517325+alwayskkc@users.noreply.github.com>
2024-11-13 17:14:21 +0000

    conditional statements

---
+++ 04_control_flow/one.js
@@ -0,0 +1,11 @@
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+let val;
+
+val = 4 ?? 1 
+console.log(val);
